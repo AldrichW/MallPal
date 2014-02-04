1527c6dc7b176984ee71ba91732a386e3eedd595
Aldrich Wingsiong <aldrich.wingsiong@gmail.com>
2014-02-03 19:50:51 -0500

    MallPal UI Initial

---
+++ Mall Directory/Mall Directory/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mall Directory
+//
+//  Created by Aldrich Wingsiong on 1/12/2014.
+//  Copyright (c) 2014 Aldrich Wingsiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

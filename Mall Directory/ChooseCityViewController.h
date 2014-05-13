13f95b3ca1bd13f56ae3866d95cd66940337fb5a
Aldrich Wingsiong <aldrichwingsiong@Aldrichs-iMac.local>
2014-05-13 16:28:43 -0700

    Fixed Build Failures where my app couldn’t detect resources

---
+++ Mall Directory/ChooseCityViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChooseCityViewController.h
+//  Mall Directory
+//
+//  Created by Aldrich Wingsiong on 5/13/14.
+//  Copyright (c) 2014 Aldrich Wingsiong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseCityViewController : UIViewController
+
+@end
---
+++ Mall Directory/ChooseCityViewController.m
@@ -0,0 +1,39 @@
+//
+//  ChooseCityViewController.m
+//  Mall Directory
+//
+//  Created by Aldrich Wingsiong on 5/13/14.
+//  Copyright (c) 2014 Aldrich Wingsiong. All rights reserved.
+//
+
+#import "ChooseCityViewController.h"
+
+@interface ChooseCityViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *Enter_Button;
+
+@end
+
+@implementation ChooseCityViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
